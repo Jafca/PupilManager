35d1a05e97b0335aada4df84757e05e9a28ed7bc
Jafca <Jafca@users.noreply.github.com>
2018-04-04 21:01:06 +0100

    Added SeedData

---
+++ PupilManager/Data/SeedData.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+using PupilManager.Models;
+
+namespace PupilManager.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new ApplicationDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<ApplicationDbContext>>()))
+            {
+                if (context.Pupils.Any())
+                {
+                    return;
+                }
+
+                context.Pupils.Add(
+                     new Pupil
+                     {
+                         FirstNames = "Jaf",
+                         LastName = "Ca",
+                         DOB = DateTime.Parse("1970-1-1"),
+                         Gender = false,
+                         MedicalConditions = null
+                     }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
